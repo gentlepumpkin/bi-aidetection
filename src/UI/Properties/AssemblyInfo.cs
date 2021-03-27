3caf2226f77581c4787a191ca650a7e85c0fe433
Chris <vorloncd@gmail.com>
2021-03-27 19:02:12 -0400

    Minor update

--- src/UI/Properties/AssemblyInfo.cs
+++ src/UI/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1024.7756")]
-[assembly: AssemblyFileVersion("2.0.1023.7756")]
+[assembly: AssemblyVersion("2.0.1025.7756")]
+[assembly: AssemblyFileVersion("2.0.1024.7756")]
