779056c5a07a990c071fb1647a2bd7b72f5adc2a
Chris <vorloncd@gmail.com>
2021-06-08 18:32:15 -0400

    - BatTest

--- src/AITool.Service/Properties/AssemblyInfo.cs
+++ src/AITool.Service/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.396.7829")]
-[assembly: AssemblyFileVersion("1.0.396.7829")]
+[assembly: AssemblyVersion("1.0.397.7829")]
+[assembly: AssemblyFileVersion("1.0.397.7829")]
